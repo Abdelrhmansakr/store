a63ae1a0f22337b8da5902d6040e1386749429f5
Abdelrhmansakr <abdelrhmansakrr@gmail.com>
2022-04-27 03:13:13 +0200

    init project

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+/* import express, { Request, Response } from 'express';
+import images from './routes/api/images';
+
+const app = express();
+const port = 3000;
+
+app.use('/api/images', images);
+
+images.get('/', (_req: Request, res: Response) : void =>  {
+  res.send('this Home go to /api/images');
+});
+
+app.listen(port, ()=> {
+  console.log(`server started at localhost:${port}`);
+});
+
+export default app; */
+
+
+console.log("firstttt");
