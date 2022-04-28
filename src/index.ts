1263c46e39c224628dae1009c6758eb30cf541d8
Abdelrhmansakr <abdelrhmansakrr@gmail.com>
2022-04-28 02:02:46 +0200

    new 1

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,7 @@ app.post('/', (req: Request, res: Response): void => {
 
 app.get('/', (req: Request, res: Response): void => {
   res.send('this Home');
-  res.json('this Home j');
+  res.json('this Home jsoon');
 });
 
 app.listen(port, () => {
